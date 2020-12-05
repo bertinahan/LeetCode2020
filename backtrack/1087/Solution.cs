0ba3dc7eb56da0277ccee0feff8be6eac445c264
Bertina Zhai <bertinaz@iqmetrix.com>
2020-12-04 18:24:08 -0800

    #1087

--- backtrack/1087/Solution.cs
+++ backtrack/1087/Solution.cs
@@ -1,39 +1,12 @@
+using System;
 public class Solution {
     public string[] Expand(string S) {
-        IList<string> res = new List<string>();
-        bt(0, S, new List<char>(), res);
-        var final = res.OrderBy(s => s).ToArray();
-        return final;
+      string s = " -43 42 string ";
+      var p = s.Split(@"(\+|-)*\d+");
+      foreach(var str in s)
+      {
+        Console.WriteLine(s);
+      }
     }
 
-    public void bt(int index, string s, IList<char> curr, IList<string> result)
-    {
-        if(index == s.Length)
-        {
-            result.Add(new string(curr.ToArray()));
-            return;
-        }
-        if(s[index] == '{')
-        {
-           int point = index+1;
-            string tmp = string.Empty;
-            while(s[point]!='}')
-            {
-               if(s[point]!=',')
-                   tmp += s[point];
-                point++;
-            }
-            foreach(var c in tmp)
-            {
-               curr.Add(c);
-                bt(point+1, s, new List<char>(curr), result);
-                curr.RemoveAt(curr.Count-1);
-            }
-        }
-        else
-        {
-            curr.Add(s[index]);
-           bt(index+1, s, curr, result);
-        }
-    }
 }
